eeba1736a2b0253fad7636ac2ac7f8cbbe8b4d98
Jeferson Bohorquez <jefersonbohorquez@gmail.com>
2021-10-11 23:17:23 +0000

    Modulo de Inventario Personalizado

---
+++ invprs/models/.ipynb_checkpoints/resuser-checkpoint.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class inventario_prs(models.Model):
+#     _name = 'inventario_prs.inventario_prs'
+#     _description = 'inventario_prs.inventario_prs'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
+
+
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
+class ResUser(models.Model):
+    _inherit = 'res.users'
+   
+    location_ids = fields.Many2many(
+        comodel_name = 'stock.picking',
+        relation ='location_user_rel',
+        column1  ='user_id',
+        column2  ='location_dest_id',
+        string   ='Ubicaciones ad Administradas',     
+    )
